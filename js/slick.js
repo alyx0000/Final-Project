a254323da2e9a636e85922a06e9b177ad9f20b56
ana <anarcheulishvili@gmail.com>
2021-07-26 01:44:09 +0400

    finished

--- js/slick.js
+++ js/slick.js
@@ -1,15 +1,10 @@
 $(function(){
 	$('.multiple-items').slick({
-	 infinite: true,
-	 // რამდენი სურათი გამოჩნდეს
-	 slidesToShow: 1,
-	 // სლაიდის დროს რამდენი ელემნტით გადავიდეს 
-	 slidesToScroll: 1,
-	 // რომ გამოჩნდეს ღილაკები
-	 arrows: true,
-	 // რომ გამოჩნდეს ბურთულები
-	 dots: false,
-	 // responsiv-ის კოდი
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: false,
 	prevArrow: '<i class="fas fa-arrow-left prev"></i>',
     nextArrow: '<i class="fas fa-arrow-right next"></i>',
 	 responsive:[
