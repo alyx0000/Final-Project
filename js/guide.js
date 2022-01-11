9f2177747339ae37588689baf90a313623e49f5a
ana <anarcheulishvili@gmail.com>
2022-01-11 20:55:10 +0400

    +

--- js/guide.js
+++ js/guide.js
@@ -18,21 +18,41 @@ function getUserInfo(page){
     responseData.data.forEach(element => {
 
         let li = document.createElement('li');
-
-        let guideName = document.createElement('span');
-        guideName.textContent = element.first_name;
         li.classList.add('guide-list');
 
-        
+        let guideImgWrapper = document.createElement('div');
+        guideImgWrapper.classList.add('guide-img_wrapper')
+
+        let guideSpanWrapper = document.createElement('div');
+        guideSpanWrapper.classList.add('guide-span_wrapper');
+
+        let guideEmailWrapper = document.createElement('div');
+        guideEmailWrapper.classList.add('guide-email_wrapper');
+
         let guideImg = document.createElement('img');
         guideImg.src = element.avatar;
         guideImg.classList.add('guide-img');
         
-        li.appendChild(guideImg);
+        let guideName = document.createElement('span');
+        guideName.textContent = element.first_name;
+
+        let guideLastname = document.createElement('span');
+        guideLastname.textContent = element.last_name;
+
+        let guideEmail = document.createElement('span');
+        guideEmail.textContent = element.email;
+        
+        guideImgWrapper.appendChild(guideImg);
+        guideSpanWrapper.appendChild(guideName);
+        guideSpanWrapper.appendChild(guideLastname);
+        guideEmailWrapper.appendChild(guideEmail)
+
+        li.appendChild(guideImgWrapper);
+        li.appendChild(guideSpanWrapper);
+        li.appendChild(guideEmailWrapper);
+
         document.getElementById('guide-ul').appendChild(li);
     })
-
-    // document.getElementById('guide-ul').appendChild(fragment);
 
     })
     .catch(function(error) { 
@@ -53,3 +73,15 @@ document.getElementById('loadMore').addEventListener('click', function() {
 });
 
 getUserInfo(currentPage);
+
+let loadMore = document.getElementById('loadMore');
+let currentUserBox = 1;
+
+loadMore.addEventListener('click', function(){
+    let userBox = document.querySelectorAll('.guide-list');
+    currentUserBox += userBox.length;
+
+    if(currentUserBox >= userBox.length){
+        event.target.style.display = 'none';
+    }
+});
